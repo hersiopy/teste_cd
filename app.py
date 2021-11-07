cca558d2764b0e85bd0d1e1aa495699fd18022b0
hersiopy <92654756+hersiopy@users.noreply.github.com>
2021-11-07 18:38:21 -0300

    teste upload5

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ def teste():
 
 @app.route("/arquivos", methods=["GET"])
 def index():
-    def index():
-        return render_template('index.html')
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
